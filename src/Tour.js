0b89510f144c5e8225badef7b18577dbb98958c0
Usama Rahman <rahmanusama390@gmail.com>
2023-10-26 06:38:06 +0600

    👍

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,9 @@
 // import React, { useState } from 'react';
 
+import { useState } from "react";
+
 const Tour = ({id, image, info, price, name}) => {
+  const [readmore, setReadmore] = useState(false)
   return(
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,6 +12,14 @@ const Tour = ({id, image, info, price, name}) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
+        <p>{readmore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadmore(!readmore)}>
+            {readmore ? 'Show less' : 'Read More' }
+          </button>
+        </p>
+        <button className="delete-btn">
+          not interested
+        </button>
       </footer>
     </article>
   )
